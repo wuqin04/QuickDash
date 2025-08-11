2ec0b677b7f12c5f05f536bd0ce90cf1f06d87fc
wuqin04 <wuqin419@gmail.com>
2025-08-11 23:54:00 +0800

    Created main.cpp and main.py

---
+++ src/python/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Welcome to QuickDash. A all-in-one personal dashboard &amp; " \
+    "reminder app with notes, tasks, and alerts at a glance.")
+
+if __name__ == "__main__":
+    main()
