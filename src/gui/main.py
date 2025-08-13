f0a1ddd33908cfd7ed9f70c888224b0f92be468e
wuqin04 <wuqin419@gmail.com>
2025-08-13 14:36:23 +0800

    Added first background into the app

--- src/gui/main.py
+++ src/gui/main.py
@@ -1,12 +1,29 @@
 import customtkinter
-from styles import set_default_theme
+from settings import set_default_theme, set_default_appearance_mode
 
+# settings 
+set_default_appearance_mode()
 set_default_theme()
-app = customtkinter.CTk()
 
-# setup the window
-app.title("QuickDash")
-app.geometry("680x400")
+# all design goes here
+# the background frame
+class BackgroundFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+# setup the app
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("QuickDash")
+        self.geometry("680x400")
+        self.resizable(width=False, height=False)
+
+        self.background_frame = BackgroundFrame(master=self, width=650, height=370, fg_color="#240046")
+        self.background_frame.place(x=15, y=15)
+
 
 # app start running here
+app = App()
 app.mainloop()
--- src/gui/settings.py
+++ src/gui/settings.py
@@ -3,5 +3,8 @@ import customtkinter
 
 default_theme_path = Path(__file__).parent.parent / "assets" / "themes" / "default.json"
 
+def set_default_appearance_mode():
+    customtkinter.set_appearance_mode("system")
+
 def set_default_theme():
     customtkinter.set_default_color_theme(default_theme_path)
