87959b9322b5b3cd8839325e94ad59fde23a62c0
wuqin04 <wuqin419@gmail.com>
2025-08-14 14:32:21 +0800

    Added quick_note_tab and rename tab_view to event_reminder_tab

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,7 +1,7 @@
 import customtkinter
 from settings import APP_TITLE, APP_SIZE, set_default_appearance_mode, set_default_theme
 from widgets.frame import BackgroundFrame, TabFrameBack, TabFrameFront
-from widgets.tab import EventReminderTab
+from widgets.tab import EventReminderTab, QuickNoteTab
 
 # settings 
 set_default_appearance_mode()
@@ -16,18 +16,25 @@ class App(customtkinter.CTk):
         self.geometry(APP_SIZE)
         self.resizable(width=False, height=False)
 
+        # Background part
         self.background_frame = BackgroundFrame(master=self)
         self.background_frame.place(x=15, y=15)
 
+        # Frame part
         self.tab_frame_back = TabFrameBack(master=self.background_frame)
         self.tab_frame_back.place(x=10, y=333)
 
         self.tab_frame_front = TabFrameFront(master=self.tab_frame_back)
         self.tab_frame_front.place(relx=0.5, rely=0.5, anchor="center")
 
-        self.tab_view = EventReminderTab(master=self.background_frame)
-        self.tab_view.place(x=20, y=321)
-        self.tab_view.lower(belowThis=self.tab_frame_back)
+        # Tab part
+        self.event_reminder_tab = EventReminderTab(master=self.background_frame)
+        self.event_reminder_tab.place(x=20, y=321)
+        self.event_reminder_tab.lower(belowThis=self.tab_frame_back)
+
+        self.quick_note_tav = QuickNoteTab(master=self.background_frame)
+        self.quick_note_tav.place(x=135, y=321)
+        self.quick_note_tav.lower(belowThis=self.tab_frame_back)
 
 # app start running here
 app = App()
--- src/gui/widgets/tab.py
+++ src/gui/widgets/tab.py
@@ -3,8 +3,15 @@ import customtkinter
 class EventReminderTab(customtkinter.CTkButton):
     def __init__(self, master, **kwargs):
         super().__init__(master, 
-                         width=96,
+                         width=98,
                          height=32,
                          text="Event Reminder",
                          **kwargs)
 
+class QuickNoteTab(customtkinter.CTkButton):
+    def __init__(self, master, **kwargs):
+        super().__init__(master,
+                         width=98,
+                         height=32,
+                         text="Quick Note",
+                         **kwargs)
