5e5147477b27c2bc682ad0038dd088dd92a41685
wuqin04 <wuqin419@gmail.com>
2025-08-16 11:19:53 +0800

    Adjusted placement of event_reminder and quick_note tab

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,7 +1,7 @@
 import customtkinter
 from settings import APP_TITLE, APP_SIZE, set_default_appearance_mode, set_default_theme
 from widgets.frames import *
-from widgets.tabs import EventReminderTab, QuickNoteTab
+from widgets.tabs import *
 from widgets.labels import *
 
 # settings 
@@ -42,18 +42,18 @@ class App(customtkinter.CTk):
 
         # Tab part
         self.event_reminder_tab = EventReminderTab(master=self.background_frame)
-        self.event_reminder_tab.place(x=20, y=321)
+        self.event_reminder_tab.place(x=20, y=324, anchor="w")
         self.event_reminder_tab.lower(belowThis=self.back_tab_frame)
 
         self.quick_note_tab = QuickNoteTab(master=self.background_frame)
-        self.quick_note_tab.place(x=135, y=321)
+        self.quick_note_tab.place(x=135, y=324, anchor="w")
         self.quick_note_tab.lower(belowThis=self.back_tab_frame)
 
         # Label here
         self.countdown_label = CountdownLabel(master=self.countdown_frame)
         self.countdown_label.place(relx=0.5, rely=0.5, anchor="center")
 
-        self.timer_label = TimerLabel(master=self.background_frame, text="5 Mins 30 Sec")
+        self.timer_label = TimerLabel(master=self.background_frame, text="5 Mins 30 Sec", text_color="#C77DFF")
         self.timer_label.place(x=350, y=30)
 
         self.top_event_label = EventLabel(master=self.top_event_frame, text="Event #1")
